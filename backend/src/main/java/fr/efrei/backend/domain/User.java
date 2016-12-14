ae0021f62e4a9001391873b6717705cb015ebca5
Pierre Filliolaud <pierre.filliolaud@gmail.com>
2016-12-14 11:11:51 +0100

    be able to add user in db

--- backend/src/main/java/fr/efrei/backend/domain/User.java
+++ backend/src/main/java/fr/efrei/backend/domain/User.java
@@ -1,12 +1,14 @@
 package fr.efrei.backend.domain;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class User {
+public class User implements Serializable {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
